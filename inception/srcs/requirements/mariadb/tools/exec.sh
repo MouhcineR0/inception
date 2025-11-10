ca997bd17e7a1bbfc89a5387e5eca4463170262d
mouhciner0 <rachidmouhcine88@gmail.com>
2025-11-10 21:32:02 +0000

    II

--- inception/srcs/requirements/mariadb/tools/exec.sh
+++ inception/srcs/requirements/mariadb/tools/exec.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-# service mariadb start
-exec mysqld_safe
 
 echo "rachid mouhcine"
+
+service mariadb start
+
+while ! mysqladmin ping --silent; do
+	sleep 1
+done
+
+mysql << limiter
+CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL ON wordpress.* TO 'rachid'@'%' IDENTIFIED BY 'rachid';
+ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'rachid';
+FLUSH PRIVILEGES;
+EXIT
+limiter
+
+mysqladmin -u root -prachid shutdown
+
+mysqladmin -u root shutdown
+
+exec mysqld_safe
