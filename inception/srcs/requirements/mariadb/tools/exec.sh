f77ef43c2b8cb23fd7696f0b55327d506551c1d5
mouhciner0 <rachidmouhcine88@gmail.com>
2025-11-11 15:09:28 +0000

    II

--- inception/srcs/requirements/mariadb/tools/exec.sh
+++ inception/srcs/requirements/mariadb/tools/exec.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-echo "rachid mouhcine"
+echo "Initializing MariaDB..."
 
-service mariadb start
+# Start MariaDB temporarily in the background
+mysqld_safe --datadir=/var/lib/mysql &
 
-while ! mysqladmin ping --silent; do
-	sleep 1
-done
+# Wait for MariaDB to be ready
+sleep 5
 
-mysql << limiter
+# Initialize database and user
+mariadb -u root <<EOF
 CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-GRANT ALL ON wordpress.* TO 'rachid'@'%' IDENTIFIED BY 'rachid';
-ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'rachid';
+CREATE USER IF NOT EXISTS 'rachid'@'%' IDENTIFIED BY 'rachid';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'rachid'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('rachid');
 FLUSH PRIVILEGES;
-EXIT
-limiter
+EOF
 
+# Stop the temporary MariaDB process cleanly
 mysqladmin -u root -prachid shutdown
 
-mysqladmin -u root shutdown
-
-exec mysqld_safe
+# Start MariaDB as the main foreground process
+exec mysqld_safe --datadir=/var/lib/mysql
---
+++ inception/srcs/requirements/wordpress/tools/wp-config.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the website, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress' );
+
+/** Database username */
+define( 'DB_USER', 'rachid' );
+
+/** Database password */
+define( 'DB_PASSWORD', 'rachid' );
+
+/** Database hostname */
+define( 'DB_HOST', 'databases' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'hLTl}w8$LR$~lbU{`T379ny_$N?tlWO<>A/8#lhU:VW<%Js%).k<7>1k~:;&cvR=' );
+define( 'SECURE_AUTH_KEY',  'HN`tO[=lbI>mr}9:^gy7?ENt@u}UCZ><d]XVgp0Ufb?vgjcv7udtvtY7-{cs~l?O' );
+define( 'LOGGED_IN_KEY',    'GlM!U>qzv<IhPG1!s5cRk]TxFU-:g,W8<jZ</h0Z|~#?I Zy1FD@lAP|QA-Tt<*j' );
+define( 'NONCE_KEY',        'DTjxU 7=U-vf<8Bn.E[<YSvH&OmMJM~D_Wpl%mD/LZK!@8D)(`T2),` x9(ipS9C' );
+define( 'AUTH_SALT',        '^LdBehc$`Zs*@7QV5x50mgMC1H|_E4:pkqy;2a=U7n>$:bzx#`{QIe}fb+f$>Zd+' );
+define( 'SECURE_AUTH_SALT', '0-4L/NT6Z})%x~4MoyX~5I%/s5PTVVqyM|gIIt$B*/+;pd^%nf1Y#tTvyl*?Unc)' );
+define( 'LOGGED_IN_SALT',   'Fe/We?~7c7tN^/88b8s2B1T-hJpnrZ]7a/.oI,P.zEi_W5c^m=2{QKJOrIFM]e @' );
+define( 'NONCE_SALT',       'U[|-Nv,ts=H`m/x3 1xT^igCiU%M8y7w|%W0?kEIN@&[:P%( !a{!SWEePGzTv#k' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ *
+ * At the installation time, database tables are created with the specified prefix.
+ * Changing this value after WordPress is installed will make your site think
+ * it has not been installed.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
