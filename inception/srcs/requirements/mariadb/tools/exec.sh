5caa2ca144c3c3917142243c2a839b62de7c3717
mouhciner0 <rachidmouhcine88@gmail.com>
2025-11-12 19:45:34 +0000

    II

--- inception/srcs/requirements/mariadb/tools/exec.sh
+++ inception/srcs/requirements/mariadb/tools/exec.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-echo "rachid mouhcine"
-
 service mariadb start
 
-# while ! mariadb ping --silent; do
 sleep 5
-# done
 
 mariadb << limiter
 CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
@@ -21,4 +17,4 @@ mysqladmin -u root -prachid shutdown
 
 service mariadb stop
 
-exec mysqld_safe --bind-address=0.0.0.0
+exec mysqld_safe
--- inception/srcs/requirements/wordpress/tools/exec.sh
+++ inception/srcs/requirements/wordpress/tools/exec.sh
@@ -14,11 +14,11 @@ wp core download --allow-root
 
 wp config create --dbname=wordpress --dbuser=rachid --dbpass=rachid --dbhost=databases --allow-root
 
-wp core install --url="localhost" --title="Inception" --admin_user=admin --admin_password=admin --admin_email=you@example.com --allow-root
+wp core install --url="https://138.197.206.36" --title="Inception" --admin_user=admin --admin_password=admin --admin_email=you@example.com --allow-root
 
-wp user create rachida regular_user@example.com --user_pass=rachida --role=author --allow-root
+# wp user create rachida regular_user@example.com --user_pass=rachida --role=author --allow-root
 
-wp option update home "https://localhost" --allow-root
-wp option update siteurl "https://localhost" --allow-root
+# wp option update home "https://localhost" --allow-root
+# wp option update siteurl "https://localhost" --allow-root
 
 php-fpm8.2 -F
